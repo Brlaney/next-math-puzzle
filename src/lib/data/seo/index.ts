dc3dcbe50b6950a69f33cdaaa00656270851b7ef
brlaney42 <brlaney@outlook.com>
2022-01-19 15:52:26 -0600

    Pushing changes to seo data

--- src/lib/data/seo/index.ts
+++ src/lib/data/seo/index.ts
@@ -1,5 +1,5 @@
 export default {
-  title: 'Math Puzzle',
-  keywords: 'Brendan Laney, web-development, civil engineer, website design, marketing, advertising, next js, react, dev, coding, programming',
-  description: 'A basic Next.js web application that uses UiKit for UI components and React-Charts-2/Chart.js for sleek plots and graphs.'
+  title: 'Sorting Puzzle',
+  keywords: 'Brendan Laney, web-development, civil engineer, website design, marketing, advertising, next js, react, dev, coding, programming, Next, React, UiKit, Scss, Sass',
+  description: 'Inspired by the mobile app WaterPuzzleSort, this is a similar type of puzzle game created with Next.js'
 };
